a57ed3754b02cd9e92a170eeecbfc71ff9e1d479
carnero <carnero@carnero.cc>
2013-06-16 20:08:43 +0200

    Basic DB structure

--- src/carnero/netmap/database/DatabaseStructure.java
+++ src/carnero/netmap/database/DatabaseStructure.java
@@ -12,9 +12,23 @@ public class DatabaseStructure {
 
 			sql.append("create table ");
 			sql.append(TABLE_BTS);
-			// TODO
+			sql.append(" (");
+			sql.append("_id integer primary key autoincrement, ");
+			sql.append("operator text not null, ");
+			sql.append("lac long not null, ");
+			sql.append("cid long not null, ");
+			sql.append("type integer default 0, ");
+			sql.append("latitude double not null, ");
+			sql.append("longitude double not null");
+			sql.append(");");
 
 			return sql.toString();
+		}
+
+		public static final String[] createBtsIndexes() {
+			return new String[] {
+					"create index if not exists idx_id on " + TABLE_BTS + " (operator, lac, cid)",
+			};
 		}
 
 		public static final String createCoverage() {
@@ -22,9 +36,22 @@ public class DatabaseStructure {
 
 			sql.append("create table ");
 			sql.append(TABLE_COVERAGE);
-			// TODO
+			sql.append(" (");
+			sql.append("_id integer primary key autoincrement, ");
+			sql.append("index_x integer not null, ");
+			sql.append("index_y integer not null, ");
+			sql.append("type integer default 0, ");
+			sql.append("signal_avg double default 0, ");
+			sql.append("signal_cnt integer default 0");
+			sql.append(");");
 
 			return sql.toString();
+		}
+
+		public static final String[] createCoverageIndexes() {
+			return new String[] {
+					"create index if not exists idx_index on " + TABLE_COVERAGE + " (index_x, index_y)",
+			};
 		}
 	}
 }
